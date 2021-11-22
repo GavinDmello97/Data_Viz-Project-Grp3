9d5124fb2888b642a6b7e74b11b8f9cfc1f05913
Gavin D'mello <gavindmello@Gavins-MacBook-Pro.local>
2021-11-22 12:47:59 -0500

    cleaned backend and added apis

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,71 +1,50 @@
-// import * as fs from 'fs';
-// import * as fs from 'fs/promises';
- 
-// import * as d3 from 'd3'
-
 const express=require('express');  // importing express using require
 const app = express(); //Creating an instance of express
 const fs = require('fs')
 const cors = require('cors');
+const axios = require('axios')
 
-//const d3 = require("d3");
+var contributorData = []
+Promise.all([axios.get("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.json")])
+.then(async response => {
+    contributorData =  response[0].data;
 
-app.get('/',(req,res)=>{
-    res.send('<h2 style="margin:2rem"> Welcome to Homepage</h2>');
-})
 
-app.get('/about',(req,res)=>{
-    res.send('<h2 style="margin:2rem"> Welcome to About us page</h2>');
-})
-
-app.get('/data',(req,res)=>{
-    res.send(data);
-})
-
-app.get("/co2-emission", cors(),(req, res) => {
-    let customArray = []
-
-    fs.readFile(__dirname + '/' + 'CO2_EmissionClean.json',"utf8",  (err, data) => {
-        let mainDataSet  = JSON.parse(data)
-        // console.log(mainDataSet[0]["Year"])
-        var unique = (src, fn) => src.filter((s => o => !s.has(fn(o)) && s.add(fn(o)))(new Set));
-        let filteredYears = unique(mainDataSet, (data) => data.Year)
-
-        let abc = []
-        
-        for (let i = 0; i< filteredYears.length; i++) {
-            if(filteredYears[i].Year !== null) {
-                abc.push( filteredYears[i].Year)
-            }
-        }
-        abc.map(elementDate => {
-            var filteredEvents = mainDataSet.filter(function(event){
-                return event.Year == elementDate;
-            });
-            // console.log("DATASET OF "+ elementDate, filteredEvents)
-            customArray.push({date: elementDate, countryList:filteredEvents})
-
-            
-        })
-        console.log(customArray)
-
-        res.send(customArray);
-
-})
-
+}).catch(error => {
+    fs.readFile(__dirname + '/data/owid-co2-data.json',"utf8",  (err, data) => {
+        contributorData = data;
+    });
 
 });
 
-/*
-var myJson = {'key' : 'value'}
-for(var myKey in myJson){
-    console.log("key: " + myKey + ", value: " + myJson[myKey]);
-}
-*/
+app.get('/',(req,res)=> res.send('<h2 style="margin:2rem"> Welcome to Homepage</h2>'))
+
+app.get("/co2-emission", cors(),(req, res) => {
+    let customCo2DatasetArray = []
+    fs.readFile(__dirname + '/data/CO2_EmissionClean.json',"utf8",  (err, data) => {
+        let mainDataSet  = JSON.parse(data)
+        var unique = (src, fn) => src.filter((s => o => !s.has(fn(o)) && s.add(fn(o)))(new Set));
+        let filteredYears = unique(mainDataSet, (data) => data.Year)
+        let tempArr = []
+        for (let i = 0; i< filteredYears.length; i++) {
+            if(filteredYears[i].Year !== null) tempArr.push(filteredYears[i].Year);
+        }
+        tempArr.map(elementDate => {
+            var filteredEvents = mainDataSet.filter(function(event){
+                return event.Year == elementDate;
+            });
+            customCo2DatasetArray.push({date: elementDate, countryList:filteredEvents})
+        })
+        res.send(customCo2DatasetArray);
+    })
+});
+
+
+app.get("/contributors", cors(),(req, res) => {
+        res.send(contributorData);
+});
+
 
 const port = process.env.PORT || 3000 //First inititalizing the port
 
-app.listen(port,()=>{
-    console.log(`Express is listening on port:${port}`)
-    
-})//Second we are going to listen to that port 
+app.listen(port,()=>  console.log(`Express is listening on port:${port}`))
